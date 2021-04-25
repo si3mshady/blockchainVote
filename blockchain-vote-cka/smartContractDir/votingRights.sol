f26cd5299b1fc2f4255cf1254dd2037cbeec75d2
si3mshady <cloudwarrior2891@gmail.com>
2021-04-24 19:55:18 -0500

    require statement is working!!

--- blockchain-vote-cka/smartContractDir/votingRights.sol
+++ blockchain-vote-cka/smartContractDir/votingRights.sol
@@ -3,22 +3,19 @@ pragma solidity >=0.4.16;
 contract District4 {
     int256 registeredVoters;
     mapping(address => Voter) public votingRegistery;
-    mapping(Candidate => uint256) public votesReceived;
+    mapping(address => uint256) public registeredAddress;
+
+    mapping(string => uint256) public votesReceived;
 
     constructor() public {
         int256 registeredVoters = 0;
     }
 
-    enum Candidate {CandidateA, CandidateB, CandidateC,  NONE}
-    Candidate constant defaultCandidate = Candidate.NONE;
-  
-
     struct Voter {
         address eth_addr; //20 byteDataType etherumAddress
         uint256 driversLicence;
         string name;
-        bytes32 ipAddress;
-        Candidate defaultCandidate;
+        string candidateName;
     }
 
     function register(uint256 _driversLicence, string memory _name)
@@ -30,19 +27,27 @@ contract District4 {
                 eth_addr: msg.sender,
                 driversLicence: _driversLicence,
                 name: _name,
-                ipAddress: "",
-                candidateChoice: ""
+                candidateName: ""
             });
 
         votingRegistery[msg.sender] = candidate;
+        registeredAddress[msg.sender] + 1;
         registeredVoters += 1;
 
         return msg.sender;
     }
 
-    function vote(Voter memory _voter, bytes32 _ipAddress, Candidate _candidate ) public returns (bool) {
-        _voter.ipAddress = _ipAddress;
+    function vote(string memory _candidateName) public {
+        // require(registeredAddress[msg.sender] > 0);
 
+        votingRegistery[msg.sender].candidateName = _candidateName;
+
+        votesReceived[_candidateName] += 1;
+
+        // return true;
     }
-
 }
+
+// https://ethereum.stackexchange.com/questions/43817/structs-mappings-enums-how-to-make-them-work-together
+//https://forum.ethereum.org/discussion/3495/operator-not-compatible-with-types-string-storage-ref-and-string-storage-ref
+//https://ethereum.stackexchange.com/questions/52215/vm-error-revert-the-transaction-has-been-reverted-to-the-initial-state?rq=1
