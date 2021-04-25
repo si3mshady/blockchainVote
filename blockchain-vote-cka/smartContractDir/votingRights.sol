9ffe2ed74855f2f6db17ff9c3e38767e466da52e
si3mshady <cloudwarrior2891@gmail.com>
2021-04-24 20:46:00 -0500

    adding new logic

--- blockchain-vote-cka/smartContractDir/votingRights.sol
+++ blockchain-vote-cka/smartContractDir/votingRights.sol
@@ -4,7 +4,7 @@ contract District4 {
     int256 registeredVoters;
     mapping(address => Voter) public votingRegistery;
     mapping(address => uint256) public registeredAddress;
-
+    mapping(address => string) public ipAddressUsed;
     mapping(string => uint256) public votesReceived;
 
     constructor() public {
@@ -12,42 +12,54 @@ contract District4 {
     }
 
     struct Voter {
-        address eth_addr; //20 byteDataType etherumAddress
+        address eth_addr;
         uint256 driversLicence;
         string name;
+        string ipAddress;
         string candidateName;
     }
 
-    function register(uint256 _driversLicence, string memory _name)
-        public
-        returns (address)
-    {
+    function register(
+        uint256 _driversLicence,
+        string memory _name,
+        string memory _ipAddress
+    ) public returns (address) {
         Voter memory candidate =
             Voter({
                 eth_addr: msg.sender,
                 driversLicence: _driversLicence,
                 name: _name,
+                ipAddress: _ipAddress,
                 candidateName: ""
             });
 
         votingRegistery[msg.sender] = candidate;
         registeredAddress[msg.sender] + 1;
         registeredVoters += 1;
+        ipAddressUsed[msg.sender] = _ipAddress;
 
         return msg.sender;
     }
 
-    function vote(string memory _candidateName) public {
-        // require(registeredAddress[msg.sender] > 0);
+    function vote(string memory _ipAddress, string memory _candidateName)
+        public
+        returns (bool)
+    {
+        //Verify IP addresss registered to voter has n
+        require(
+            keccak256(bytes(ipAddressUsed[msg.sender])) ==
+                keccak256(bytes(_ipAddress))
+        );
 
+        votingRegistery[msg.sender].ipAddress = _ipAddress;
         votingRegistery[msg.sender].candidateName = _candidateName;
 
         votesReceived[_candidateName] += 1;
 
-        // return true;
+        return true;
     }
 }
 
 // https://ethereum.stackexchange.com/questions/43817/structs-mappings-enums-how-to-make-them-work-together
 //https://forum.ethereum.org/discussion/3495/operator-not-compatible-with-types-string-storage-ref-and-string-storage-ref
-//https://ethereum.stackexchange.com/questions/52215/vm-error-revert-the-transaction-has-been-reverted-to-the-initial-state?rq=1
+//https://ethereum.stackexchange.com/questions/30912/how-to-compare-strings-in-solidity/82739
