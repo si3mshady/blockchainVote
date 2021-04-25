88e7b98b8aee74f3a8d2a8d5528c15f9d73e235b
si3mshady <cloudwarrior2891@gmail.com>
2021-04-25 14:57:28 -0500

    calling functions from blockchain

--- blockchain-vote-cka/src/components/Register.js
+++ blockchain-vote-cka/src/components/Register.js
@@ -11,6 +11,8 @@ export default function Register({ip}) {
     const [account, setAccount] = useState('');
     const [abi, setAbi] = useState('');
     const [web3Instance, setWeb3Instance] = useState();
+    const [actualVoters, setActualVoters] = useState();
+    const [registeredVoters, setRegisteredVoters] = useState();
     const district4_SC =  "0x1a3e43492533a45c7785ce0b9f45297ca8e08718";
   
   
@@ -48,9 +50,15 @@ export default function Register({ip}) {
    contract_instance.methods.register(driversLicenceInt, fullName, ipString).send({from: account})
    .then(res => {
    
-    let response = contract_instance.methods.getActualVoters().call()
+    let response = contract_instance.methods.getRegisteredVoters().call()
     .then(data => {
-        console.log(data)
+        console.log(
+        
+            setRegisteredVoters(data)
+            
+            )
+
+
     })
    
    }
@@ -75,6 +83,9 @@ return (
         <div className="register">
 
 <p>Your IP address is <span style={{color:"green"}}>{ip}</span></p> 
+
+
+{registeredVoters && <h1>Registered Voters: <span style={{color:"green"}}> {registeredVoters} </span> </h1> } 
 
 <Form.Group className="">
   <Form.Row>
@@ -102,7 +113,9 @@ return (
   </div>
  
 </Form.Group>
+ 
         
+
 
             
         </div>
--- blockchain-vote-cka/src/components/Vote.js
+++ blockchain-vote-cka/src/components/Vote.js
@@ -1,13 +1,80 @@
-import React from 'react'
 import { Form, Col, Button } from 'react-bootstrap';
-export default function Vote() {
+import React, {useState, useEffect} from 'react'
+import Web3 from 'web3';
+import './css/Vote.css'
+import compiled_contract from './abi.json'
+
+
+export default function Vote({ip}) {      
+   
+    const [candidateName, setCandidateName] = useState('');
+    const [account, setAccount] = useState('');
+    const [abi, setAbi] = useState('');
+    const [web3Instance, setWeb3Instance] = useState();
+    const [actualVoters, setActualVoters] = useState();
+    // const [registeredVoters, setRegisteredVoters] = useState();
+    const district4_SC =  "0x1a3e43492533a45c7785ce0b9f45297ca8e08718";
+
+    useEffect(() => {
+        const initializeWeb3 = async () => {
+          try {
+            // Will open the MetaMask UI       
+           const accounts =  await window.ethereum.request({ method: 'eth_requestAccounts' });       
+           let web3 = new  Web3(window.ethereum);  
+           window.ethereum.enable()  
+           setAccount(accounts[0])  
+           setWeb3Instance(web3)  
+           console.log(web3Instance)  
+    
+          } catch (error) {
+            console.error(error);
+          }
+        };
+        initializeWeb3()
+        setAbi(compiled_contract.abi)
+      
+        return true;
+      },[])  
+
+    
+      const vote =  () => {   
+
+        let ipString = new String(ip)       
+       let contract_instance =  new web3Instance.eth.Contract(abi,district4_SC)
+       contract_instance.methods.vote(ipString, candidateName).send({from: account})
+       .then(res => {
+          let response = contract_instance.methods.getActualVoters().call()
+        .then(data => {
+            console.log( setActualVoters(data) )   
+    
+        })       
+       }      
+    
+       )    
+        
+      }
+    
+        const handleCandidateName = (e) => {
+            setCandidateName(e.target.value)
+            
+        }
+        
+
+
+
     return (
         <div className="vote">
+
+{actualVoters && <h1>Actual Voters: <span style={{color:"green"}}> {actualVoters} </span> </h1> } 
+
             <Form.Group className="">
   <Form.Row>
 
     <Col>
-      <Form.Control size="lg" type="text" placeholder="Candidate Name" />
+      <Form.Control
+       onChange={handleCandidateName}
+      value={candidateName}
+       size="lg" type="text" placeholder="Candidate Name" />
     </Col>
   </Form.Row>
   <br />
@@ -15,7 +82,7 @@ export default function Vote() {
     
   <br />
   <div className="register_button">
-        <Button >Vote</Button>
+        <Button onClick={() => vote()} >Vote</Button>
   </div>
  
 </Form.Group>
